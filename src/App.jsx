e2d07234e198a9caf1443685e75a3ce87ae32e3e
dhilip143 <kdhilip146@gmail.com>
2025-11-17 11:20:55 +0530

    want

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import About from "./Components/About";
-import Education from "./Components/education";
+import Education from "./src/Components/Education.jsx";
 import Experience from "./Components/experience";
 import Landing from "./Components/Landing";
 import Projects from "./Components/projects";
